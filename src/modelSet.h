fc82d1310b8ea923c6da085819f59013fcba185a
mzed <github@mikezed.com>
2016-11-01 12:30:05 +0000

    Documenting with DOxygen

--- src/modelSet.h
+++ src/modelSet.h
@@ -15,8 +15,11 @@ class modelSet {
 public:
     modelSet();
     ~modelSet();
+    /** Train on a specified set, causes creation if not created */
     bool train(std::vector<trainingExample> trainingSet);
+    /** reset to pre-training state */
     bool initialize();
+    /** run regression or classification for each model */
     std::vector<double> process(std::vector<double> inputVector);
     
 protected:
@@ -27,9 +30,13 @@ protected:
 
 #ifndef EMSCRIPTEN //The javascript code will do its own JSON parsing
 public:
+    /** Get a JSON representation of the model in the form of a styled string */
     std::string getJSON();
+    /** Write a JSON model description to specified file path */
     void writeJSON(std::string filepath);
+    /** configure empty model with string. See getJSON() */
     bool putJSON(std::string jsonMessage);
+    /** read a JSON file at file path and build a modelSet from it */
     bool readJSON(std::string filepath);
     
 private:
