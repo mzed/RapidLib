bbad14cdf63f611a3af209abab56f904b2a6c310
mzed <github@mikezed.com>
2017-12-05 12:51:16 +0000

    Revert "new docs files"
    This reverts commit 8c8d7efee446cd54350b9228d6e2b135787b1f43.

--- docs_doxygen/html/search/typedefs_3.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['trainingexample',['trainingExample',['../training_example_8h.html#a0bb207589af8d149b05ad13bc092c71f',1,'trainingExample.h']]],
-  ['trainingexamplefloat',['trainingExampleFloat',['../training_example_8h.html#a2cd923732ec41c40c0d938ceffc690fa',1,'trainingExample.h']]],
-  ['trainingseries',['trainingSeries',['../training_example_8h.html#a698f8109e2a60bfa71137ce4e4de5be1',1,'trainingExample.h']]],
-  ['trainingseriesfloat',['trainingSeriesFloat',['../training_example_8h.html#a807bcf4d76d86c046102aa48fc54e036',1,'trainingExample.h']]]
-];
